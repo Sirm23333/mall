1197446a91c3ee024aacb7fe0eede20c915614d3
sirm <sunyuping2018@163.com>
2020-12-22 19:15:49 +0800

    'conf:配置nacos服务注册'

--- mall-product/src/main/java/com/sirm/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/sirm/mall/product/MallProductApplication.java
@@ -3,9 +3,11 @@ package com.sirm.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.sirm.mall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {
